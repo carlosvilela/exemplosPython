e0e44ead4fde70ac9f32b77fa1ea1a77d2de5222
Carlos Vilela <carlosvilela@users.noreply.github.com>
2020-05-09 16:45:55 -0300

    Create questao_03.py

---
+++ questao_03.py
@@ -0,0 +1,20 @@
+#+-----------------------------------------------------------------+
+#|                                                   questao_01.py |
+#|                          Copyright 2020, Carlos Bezerra Vilela. |
+#|                  https://github.com/carlosvilela/exemplosPython |
+#+-----------------------------------------------------------------+
+
+# Entrada de dados
+my_floats = [5.75, 8.09, 3.25, 20.55, 3.15, 8.08, 5.09]
+
+# função para arredondar 2 casas decimais
+def arredondarQuadradoValorEmDuasCasas (entradaDados):
+    numeroCasaDecimal = 5
+    arredondar = list (map(lambda x: round(x**2,2), entradaDados))
+    return arredondar
+
+# Resultado
+map_result = arredondarQuadradoValorEmDuasCasas(my_floats)
+
+# imprimir valores
+print(map_result)
